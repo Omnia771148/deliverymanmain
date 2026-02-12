8e588d175a9b37a3b6e6fcc35d991f4be8ff4d48
omnia771148 <omnia771148@gmail.com>
2026-02-12 20:16:27 +0530

    Initial commit

--- src/app/components/OSMMap.js
+++ src/app/components/OSMMap.js
@@ -22,12 +22,10 @@ const destinationIcon = L.icon({
 });
 
 const currentIcon = L.icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
-    shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
+    iconUrl: '/delivery.png', // Using the delivery.png from public folder
+    iconSize: [50, 50],
+    iconAnchor: [25, 25],
+    popupAnchor: [0, -25]
 });
 
 // Component to handle the routing logic
