c00e45cf82c10f66611caaf0ddb0ac8ca4261dd2
omnia771148 <omnia771148@gmail.com>
2026-03-25 23:55:46 +0530

    initial commit

--- src/app/deliveryboy/forgot-password/page.js
+++ src/app/deliveryboy/forgot-password/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { auth } from "../../../../lib/firebase";
 import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 import { useRouter } from "next/navigation";
--- src/app/deliveryboy/signup/page.js
+++ src/app/deliveryboy/signup/page.js
@@ -8,7 +8,7 @@ import Loading from "../../loading/page";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './signup.css';
 
-// SVG Icons
+// SVG 
 const UserIcon = () => (
   <svg className="input-icon" viewBox="0 0 24 24" fill="#aaa">
     <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
